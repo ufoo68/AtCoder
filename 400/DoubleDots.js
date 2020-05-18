3607141fbb5d1d0012edcd801b563aed59f0420e
ufoo68 <mitarou.yuutarou@gmail.com>
2020-05-18 23:34:22 +0900

    ABC-168

--- 400/DoubleDots.js
+++ 400/DoubleDots.js
@@ -3,6 +3,30 @@ const main = (input) => {
   const nm = args[0].split(' ').map(v => Number(v))
   const n = nm[0]
   const m = nm[1]
-  const lines = args.slice(1, n)
+  const lines = args.slice(1, args.length).map(line => line.split(' ').map(v => Number(v)))
+  const room = Array(n + 1).fill(0).map(() => [])
+  for (let i = 0; i < m; i++) {
+    const [a, b] = lines[i]
+    room[a].push(b)
+    room[b].push(a)
+  }
+  let prev = Array(n).fill(-1)
+  prev[0] = 0
+  const queue = []
+  queue.push(1)
+  while (queue.length > 0) {
+    const p = queue.shift()
+    room[p].forEach(n => {
+      if (prev[n - 1] !== -1) {
+        return
+      }
+      prev[n - 1] = p
+      queue.push(n)
+    })
+  }
+  console.log('Yes')
+  for (let i = 1; i < n; i++) {
+    console.log(prev[i])
+  }
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'))
